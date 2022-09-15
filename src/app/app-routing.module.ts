d79335f28660e4e733921344632e694d1ff853cd
dev-apiarist <klesliedev@gmail.com>
2022-09-15 09:59:32 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ServicesComponent } from './pages/services/services.component'
+import { ProductsComponent } from './pages/users/products/products.component'
+import { CartComponent } from './pages/users/cart/cart.component'
+import { HomeComponent } from './pages/home/home.component'
 import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
+import { Router, RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [
+	{ path: 'home', component: HomeComponent },
+	{ path: 'products', component: ProductsComponent },
+	{ path: 'services', component: ServicesComponent },
+	{ path: 'cart', component: CartComponent },
+	{ path: '*', redirectTo: 'home', pathMatch: 'full' },
+	{ path: '**', redirectTo: 'home' },
+]
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
